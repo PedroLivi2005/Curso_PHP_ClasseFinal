7da35f07061811810706472ac2f9267673a0f000
unknown <pedro.livi2005@gmail.com>
2026-04-27 17:26:25 -0300

    3

---
+++ aula6-arrays/string.php
@@ -0,0 +1,13 @@
+<?php 
+
+$name = "Pedro";
+$greeting = "Seja bem vindo ao curso \"\"";
+$greeting = 'Seja bem vindo ao curso \'\'';
+$today = 27;
+
+$message = "Hello world " . $greeting . $name . " hoje é dia " . $today;
+
+$test = 'Hello world $greeting $name hoje é dia ';
+
+echo $test;
+
---
+++ aula7-funções.php/funcoes.php
@@ -0,0 +1,19 @@
+<?php 
+
+$name = "Pedro";
+$greeting = "Seja bem vindo ao curso \"\"";
+$greeting = 'Seja bem vindo ao curso \'\'';
+$today = 27;
+
+$message = "Hello world " . $greeting . $name . " hoje é dia " . $today;
+$test = 'Hello world $greeting $name hoje é dia ';
+//echo $test;
+
+//Funções em PHP
+//funçao(argumento)
+
+//date();
+//var_dump($message);
+//print('Olá');
+
+echo date('d/m/Y');
