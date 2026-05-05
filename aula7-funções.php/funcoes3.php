9698d68f3a047e8652ea975f35aa27b29b58a7c7
unknown <pedro.livi2005@gmail.com>
2026-05-05 17:31:37 -0300

    7

---
+++ aula7-funções.php/funcoes3.php
@@ -0,0 +1,43 @@
+<?php
+//Como receber uma função por parâmetro para utilizar dentro de outra função
+
+declare(strict_types=1);
+
+//Passando uma função anônima como um argumento. (Callback)
+function sum(int $num1, int $num2, Closure $fn): void{
+    echo ($num1 + $num2) . PHP_EOL;
+
+    $fn(); //Executa o callback recebido por parâmetro
+}
+
+function sum2(int $num1, int $num2, Closure $fn): void{
+    $result = $num1 + $num2;
+
+    $fn($result); //Executa o callback recebido por parâmetro
+}
+
+$print = fn(int $result) => print("Cálculo finalizado e o resultado é $result");
+
+function show(string $text, Closure $cleaner): void{
+    var_dump($cleaner($text));
+}
+
+show('      Hello World      ', function(string $text): string {
+    return trim($text);
+});
+
+//sum2(1,2, $print);
+
+//Invocação da função de ordem superior passando uma Closure (função anônima)
+/*
+sum(1, 2, function() {
+    echo 'Cálculo finalizado';
+});*/
+
+/*
+function show(): void{
+    echo 'O cálculo foi feito';
+}*/
+
+/* Versão simplificada para um função de uma única linha
+sum(1, 2, fn() => print('Cálculo finalizado'));*/
---
+++ aula7-funções.php/variádicos.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1); //habilitar o modo de tipagem estrita
+
+//1- Dessa forma com o array a função vai aceitar parâmetros de diferentes tipos, mas irá apresentar erro se ouver cálculos:
+/*
+function sum(array $numbers): void{
+    $numbers = func_get_args();//Essa função retorna um array contendo todos os argumentos passados para uma função definida pelo usuário
+    var_dump($numbers);
+}
+sum([1,2,3,4, 'c']);
+*/
+
+//2- Restringir os parâmetros de mesmo tipo
+//O parâmetro que receber argumentos variádicos "...", deve ser o último
+function sum(int $exponent, int $divisor, int|float ...$numbers): void{
+    var_dump($numbers);
+}
+
+sum(1, 2, 3, 4, 6.5);
